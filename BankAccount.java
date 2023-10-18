3b69cdd081fa216bc2be7f71e636e9993e95d69a
Onur Kayhan <onur.kayhan@chasacademy.se>
2023-10-18 23:19:59 +0200

    add deposit and withdraw method

--- BankAccount.java
+++ BankAccount.java
@@ -2,13 +2,29 @@ public class BankAccount {
     private int accountNumber;
     private double balance;
 
-    public BankAccount(int accountNumber) {
+    public BankAccount(int accountNumber, double balance) {
         this.accountNumber = accountNumber;
-        // this.balance = balance;
+        this.balance = balance;
     }
 
     public int getAccountNumber() {
         return this.accountNumber;
+    }
+
+    public void withdraw(double amount) {
+
+        if (balance <= 0)
+            System.out.println("Du har inte tillräckligt med pengar.");
+
+        else
+            System.out.println("Du lyckades att ta ut " + amount + "kr.");
+        this.balance -= amount;
+
+    }
+
+    public void deposit(double depositAmount) {
+        System.out.println("Du satte in " + depositAmount + "kr.");
+        this.balance = balance + depositAmount;
     }
 
     public String toString() {
--- Register.java
+++ Register.java
@@ -8,26 +8,42 @@ public class Register {
     private int accountNumber;
     private boolean isAccount;
     private int checkAccount;
-    private boolean secondMenu = true;
+    private boolean secondMenu;
+    private boolean isExist;
 
     ArrayList<BankAccount> listOfAccounts = new ArrayList<>();
 
     public void addAccount() {
+        isExist = false;
 
         System.out.print("Ange kontonummer> ");
         accountNumber = scanner.nextInt();
 
-        listOfAccounts.add(new BankAccount(accountNumber));
-
         // Show all accounts --> for the teacher to debug, shows the array
         for (BankAccount account : listOfAccounts) {
             System.out.println(account.toString());
+
+            if (accountNumber == account.getAccountNumber()) {
+                isExist = true;
+            }
+
         }
+
+        if (!isExist) {
+            System.out.println("Kontot skapades, grattis!");
+            listOfAccounts.add(new BankAccount(accountNumber, 0));
+            isExist = false; // kan också vara false - diskuterar sen med läraren
+        } else {
+            System.out.println("Kontot existerar redan, försök igen: ");
+            isExist = true;
+        }
+
     }
 
     public void manageAccount() {
         System.out.print("Ange kontonummer> ");
         checkAccount = scanner.nextInt();
+
         isAccount = false;
 
         for (int i = 0; i < listOfAccounts.size(); i++) {
@@ -47,11 +63,17 @@ public class Register {
 
                     switch (userChoice) {
                         case 1:
-                            System.out.println("Du valde att ta ut pengar");
+                            System.out.print("Ange belopp att ta ut: ");
+                            double amount = scanner.nextDouble();
+                            listOfAccounts.get(i).withdraw(amount);
+                            secondMenu = true;
                             break;
 
                         case 2:
-                            System.out.println("Du valde att sätta in pengar");
+                            System.out.print("Ange belopp att sätta in: ");
+                            double depositAmount = scanner.nextDouble();
+                            listOfAccounts.get(i).deposit(depositAmount);
+                            secondMenu = true;
                             break;
 
                         case 3:
@@ -62,9 +84,12 @@ public class Register {
                             System.out.println("Du valde att avsluta. Ha det bra");
                             secondMenu = false;
                             break;
-                    }
 
-                    break;
+                        default:
+                            System.out.println("Du måste ange val i menyn.");
+                            secondMenu = true;
+                            break;
+                    }
 
                 } while (secondMenu);
 
